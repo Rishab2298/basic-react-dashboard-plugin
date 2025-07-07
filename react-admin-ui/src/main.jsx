32a64de5aa46b2087246f63337febca4044d5a7b
Rishab Jain <rjain22061998@gmail.com>
2025-07-08 00:12:34 +0530

    final plugin update

--- react-admin-ui/src/main.jsx
+++ react-admin-ui/src/main.jsx
@@ -1,28 +1,28 @@
-// import React from "react";
-// import ReactDOM from "react-dom/client";
-// import App from "./App";
-// import "./index.css"; // Required to trigger app.css build
-
-
-// document.addEventListener("DOMContentLoaded", () => {
-  
-//   const root = document.getElementById("root");
-//   if (root) {
-//     ReactDOM.createRoot(root).render(<App />);
-//   } else {
-//     console.error("❌ No #react-admin-root element found. Make sure your WordPress plugin outputs <div id='react-admin-root'></div> in the admin page.");
-
-//   }
-// });
-
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "./index.css";
+import "./index.css"; // Required to trigger app.css build
 
-const root = document.getElementById("root");
-if (root) {
-  ReactDOM.createRoot(root).render(<App />);
-} else {
-  console.error("❌ No #root element found.");
-}
+
+document.addEventListener("DOMContentLoaded", () => {
+  
+  const root = document.getElementById("root");
+  if (root) {
+    ReactDOM.createRoot(root).render(<App />);
+  } else {
+    console.error("❌ No #react-admin-root element found. Make sure your WordPress plugin outputs <div id='react-admin-root'></div> in the admin page.");
+
+  }
+});
+
+// import React from "react";
+// import ReactDOM from "react-dom/client";
+// import App from "./App";
+// import "./index.css";
+
+// const root = document.getElementById("root");
+// if (root) {
+//   ReactDOM.createRoot(root).render(<App />);
+// } else {
+//   console.error("❌ No #root element found.");
+// }
